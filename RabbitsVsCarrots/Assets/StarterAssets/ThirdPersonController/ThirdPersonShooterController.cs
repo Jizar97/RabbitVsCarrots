efc2c9640af387a8557b64cb9538bc75edc2d74e
Jizar97 <45200712+Jizar97@users.noreply.github.com>
2021-11-17 21:43:45 -0300

    Mecanicas

---
+++ RabbitsVsCarrots/Assets/StarterAssets/ThirdPersonController/ThirdPersonShooterController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ThirdPersonShooterController : MonoBehaviour{
+
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+
+    private void Update() {
+        
+    }
+
+}
