3a0d5de1c6415d10b8509f487b5b5c124b4286ad
Jizar97 <45200712+Jizar97@users.noreply.github.com>
2021-11-24 17:20:14 -0300

    ASdf

--- RabbitsVsCarrots/Assets/Scripts/Vitoria.cs
+++ RabbitsVsCarrots/Assets/Scripts/Vitoria.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOver : MonoBehaviour
+
+
+public class Vitoria : MonoBehaviour
 {
     
+    AudioSource animationSoundPlayer;
+
     public void Setup() {
         gameObject.SetActive(true);
+        animationSoundPlayer = GetComponent<AudioSource>();
+        animationSoundPlayer.Play();
     }
 
     public void Restart(){
