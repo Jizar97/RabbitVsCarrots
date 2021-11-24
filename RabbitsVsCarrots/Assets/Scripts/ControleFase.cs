7eec1d904f6f812e5a46559a8d3554f8ffd75593
Jizar97 <45200712+Jizar97@users.noreply.github.com>
2021-11-24 16:27:16 -0300

    Sistema de Waves V1

--- RabbitsVsCarrots/Assets/Scripts/ControleFase.cs
+++ RabbitsVsCarrots/Assets/Scripts/ControleFase.cs
@@ -7,38 +7,65 @@ public class ControleFase : MonoBehaviour
 
 
     [SerializeField]
-    public Spawner spawner;
+    public Spawner spawner1;
+    [SerializeField]
+    public Spawner spawner2;
+    [SerializeField]
+    public Spawner spawner3;
+    [SerializeField]
+    public Spawner spawner4;
+
     [SerializeField]
     public UIInimigosVivos uiInimCount;
     [SerializeField]
     public UIOndaAtual uiWaveCount;
 
     private int onda, inimigosVivos;
+    private bool bloqueia = true;
 
     // Start is called before the first frame update
     void Start()
     {
         onda = 1;
+        StartCoroutine(Espera());
     }
 
     // Update is called once per frame
     void Update()
-    {
-        inimigosVivos = GameObject.FindGameObjectsWithTag("Enemy").Length;
-        inimigosVivos = inimigosVivos - 1;
+    {   
+        if(bloqueia == false){
+            inimigosVivos = GameObject.FindGameObjectsWithTag("Enemy").Length;
+            inimigosVivos = inimigosVivos - 1;
 
-        uiInimCount.SetNum(inimigosVivos);
+            uiInimCount.SetNum(inimigosVivos);
 
-        //Debug.Log ("Inimigos vivos: " + inimigosVivos);
+            //Debug.Log ("Inimigos vivos: " + inimigosVivos);
 
-        if(inimigosVivos == 0){
+            if(inimigosVivos == 0){
+
             if(onda == 5){
-
+                Vitoria();
             } else {
-                onda += 1;
+                onda++;
                 uiWaveCount.SetNum(onda);
-                spawner.Spawn(onda);
+                StartCoroutine(Espera());
+                bloqueia = true;
             }
         }
+        }
+    }
+
+    IEnumerator Espera(){
+        Debug.Log ("Esperando....");
+        yield return new WaitForSeconds(5);
+        spawner1.Spawn(onda);
+        spawner2.Spawn(onda);
+        spawner3.Spawn(onda);
+        spawner4.Spawn(onda);
+        bloqueia = false;
+    }
+
+    void Vitoria(){
+        
     }
 }
--- RabbitsVsCarrots/Assets/Scripts/EnemyHealth.cs
+++ RabbitsVsCarrots/Assets/Scripts/EnemyHealth.cs
@@ -37,7 +37,7 @@ public class EnemyHealth : MonoBehaviour
     void OnCollisionEnter(Collision collision){
 
         if(collision.gameObject.name == "Bullet2(Clone)"){
-            TakeDamage(20);
+            TakeDamage(100);
             toggle.HitMarker();
             //Instantiate(vfxHit, transform.position, Quaternion.identity);
         }
@@ -53,7 +53,7 @@ public class EnemyHealth : MonoBehaviour
     }
 
     IEnumerator Destroi(){
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(1);
         Destroy(gameObject);
     }
 }
