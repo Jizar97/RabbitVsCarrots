33ccfbf24829da5110b9e4baef0817c34bcd637e
Jizar97 <45200712+Jizar97@users.noreply.github.com>
2021-11-28 14:59:54 -0300

    Hacking 1.2

--- RabbitsVsCarrots/Assets/Scripts/TelaHacking.cs
+++ RabbitsVsCarrots/Assets/Scripts/TelaHacking.cs
@@ -1,21 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TelaHacking : MonoBehaviour
 {
 
     public Barreira barreira;
+    public InputField input;
+    public Text pergunta;
+
+    string resultado;
+
+    bool passou;
     
     public void Setup() {
         gameObject.SetActive(true);
         Cursor.lockState = CursorLockMode.None;
+        Inicio();
 
+    }
+
+    public void Update() {
+        
+    }
+
+    public void Inicio() {
+
+        pergunta.text = ("8 * 3 = ?");
+
+        while (resultado != "24")
+        {
+            resultado = input.text;
+        }
+
+        pergunta.text = ("8 * 3 = ?");
+
+        if(resultado == "24"){
+            passou = true;
+        } else {
+
+        }
+
+        pergunta.text = ("12 + 4 * 2 = ?");
+
+        if(resultado == "20"){
+            passou = true;
+        } else {
+
+        }
+
+        pergunta.text = ("(3 - 5) * 2 = ?");
+
+        if(resultado == "4"){
+            passou = true;
+        } else {
+
+        }
     }
     
     public void Hackear(){
-        barreira.Desativar();
+
+        if(passou == true){
+            barreira.Desativar();
+        }
     }
 
     public void Exit(){
