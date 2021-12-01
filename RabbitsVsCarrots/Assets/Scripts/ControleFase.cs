5ac473e250de934de523af958a100e6ff9650970
Jizar97 <45200712+Jizar97@users.noreply.github.com>
2021-12-01 07:55:21 -0300

    Levels interligados

--- RabbitsVsCarrots/Assets/Scripts/ControleFase.cs
+++ RabbitsVsCarrots/Assets/Scripts/ControleFase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControleFase : MonoBehaviour
 {
@@ -19,23 +20,30 @@ public class ControleFase : MonoBehaviour
     public UIInimigosVivos uiInimCount;
     [SerializeField]
     public UIOndaAtual uiWaveCount;
-    [SerializeField]
-    public Vitoria vitoria;
+
+    public AudioClip batalha, vitoria;
+
+    public Desativador mostrarTexto;
+    public Desativador barraDeVida;
+    public Desativador inimigosUI;
+    public Desativador npc;
+    public Text texto;
 
     private int onda, inimigosVivos;
-    private bool bloqueia = true;
+    private bool bloqueia = true, fim = false;
 
     // Start is called before the first frame update
     void Start()
     {
         onda = 1;
-        StartCoroutine(Espera());
+        StartCoroutine(Onda1());
+        GetComponent<AudioSource> ().clip = batalha;
     }
 
     // Update is called once per frame
     void Update()
     {   
-        if(bloqueia == false){
+        if(fim == false && bloqueia == false){
             inimigosVivos = GameObject.FindGameObjectsWithTag("Enemy").Length;
             inimigosVivos = inimigosVivos - 1;
 
@@ -46,20 +54,37 @@ public class ControleFase : MonoBehaviour
             if(inimigosVivos == 0){
 
             if(onda == 5){
-                Victory();
+                StartCoroutine(Vitoria());
             } else {
                 onda++;
                 uiWaveCount.SetNum(onda);
-                StartCoroutine(Espera());
+                barraDeVida.Desativar();
+                inimigosUI.Desativar();
+                StartCoroutine(OndaX());
                 bloqueia = true;
             }
         }
         }
     }
 
-    IEnumerator Espera(){
-        Debug.Log ("Esperando....");
+    IEnumerator Onda1(){
         yield return new WaitForSeconds(5);
+        mostrarTexto.Reativar();
+        yield return new WaitForSeconds(10);
+        mostrarTexto.Desativar();
+        yield return new WaitForSeconds(2);
+        mostrarTexto.Reativar();
+        texto.text = "3";
+        yield return new WaitForSeconds(1);
+        texto.text = "2";
+        yield return new WaitForSeconds(1);
+        texto.text = "1";
+        yield return new WaitForSeconds(1);
+        texto.text = "AGORA!!";
+        yield return new WaitForSeconds(1);
+        mostrarTexto.Desativar();
+        barraDeVida.Reativar();
+        inimigosUI.Reativar();
         spawner1.Spawn(onda);
         spawner2.Spawn(onda);
         spawner3.Spawn(onda);
@@ -67,8 +92,64 @@ public class ControleFase : MonoBehaviour
         bloqueia = false;
     }
 
-    void Victory(){
-        Cursor.lockState = CursorLockMode.None;
-        vitoria.Setup();
+    IEnumerator OndaX(){
+        yield return new WaitForSeconds(1);
+        mostrarTexto.Reativar();
+        texto.text = "SOBREVIVEU A ONDA: " + (onda - 1);
+        yield return new WaitForSeconds(2);
+        mostrarTexto.Desativar();
+        yield return new WaitForSeconds(1);
+        mostrarTexto.Reativar();
+        texto.text = "PREPARE-SE PARA A PROXIMA ONDA";
+        yield return new WaitForSeconds(3);
+        mostrarTexto.Desativar();
+        yield return new WaitForSeconds(2);
+        mostrarTexto.Reativar();
+        texto.text = "3";
+        yield return new WaitForSeconds(1);
+        texto.text = "2";
+        yield return new WaitForSeconds(1);
+        texto.text = "1";
+        yield return new WaitForSeconds(1);
+        texto.text = "AGORA!!";
+        yield return new WaitForSeconds(1);
+        mostrarTexto.Desativar();
+        barraDeVida.Reativar();
+        inimigosUI.Reativar();
+        spawner1.Spawn(onda);
+        spawner2.Spawn(onda);
+        spawner3.Spawn(onda);
+        spawner4.Spawn(onda);
+        bloqueia = false;
+
+    }
+
+    IEnumerator Vitoria(){
+        yield return new WaitForSeconds(1);
+        barraDeVida.Desativar();
+        inimigosUI.Desativar();
+        mostrarTexto.Reativar();
+        texto.text = "VITORIA!!!";
+        VictorySound();
+        //yield return new WaitForSeconds(5);
+        //mostrarTexto.Desativar();
+    }
+
+    private void VictorySound(){
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().volume = 1.0f;
+        GetComponent<AudioSource> ().PlayOneShot(vitoria);
+        fim = true;
+        bloqueia = true;
+        StartCoroutine(Espera(3));
+        mostrarTexto.Desativar();
+        npc.Reativar();
+
+
+    }
+
+    IEnumerator Espera(int tempo){
+        yield return new WaitForSeconds(tempo);
+        bloqueia = false;
     }
 }
---
+++ RabbitsVsCarrots/Assets/Scripts/NPC.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NPC : Interactable {
+
+    public Desativador balao;
+    public Desativador dialogo;
+    public Desativador crosshair;
+    public Desativador textUI;
+    public Text texto;
+    public Transform Player;
+
+
+    private void Start() {
+        
+    }
+
+
+    public override string GetDescription() {
+        return "Pressione [E] para falar";
+    }
+
+    public override void Interact() {
+        transform.LookAt(Player);
+        balao.Desativar();
+        dialogo.Reativar();
+        crosshair.Desativar();
+        textUI.Desativar();
+        StartCoroutine(Espera());
+    }
+
+    IEnumerator Espera(){
+        yield return new WaitForSeconds(3);
+        texto.text = "Se você não me matar nós podemos fazer um trato. Que tal?";
+        yield return new WaitForSeconds(4);
+        texto.text = "Eu posso te levar até a rainha.";
+        yield return new WaitForSeconds(5);
+        texto.text = "É isso que você quer, certo? Matar a rainha para acabar com a infestação.";
+        yield return new WaitForSeconds(5);
+        texto.text = "Eu te ajudo, eu não gosto dela mesmo.";
+        yield return new WaitForSeconds(5);
+        texto.text = "Para matá-la você vai precisar passar pela antiga barreira militar.";
+        yield return new WaitForSeconds(5);
+        texto.text = "Você vai ter de hackear o sistema de defesa no terminal de acesso.";
+        yield return new WaitForSeconds(5);
+        texto.text = "Mas não se preocupe, eu já pensei em tudo. Aqui, toma.";
+        yield return new WaitForSeconds(5);
+        texto.text = "Neste pendrive está o meu vírus, é só espetar ele no terminal.";
+        yield return new WaitForSeconds(5);
+        texto.text = "Bem... você ainda vai precisar passar pelas três camadas de proteção.";
+        yield return new WaitForSeconds(5);
+        texto.text = "Mas deve ser fácil o suficiente até para um coelho caipira.";
+        yield return new WaitForSeconds(5);
+        texto.text = "Ok, eu vou levá-lo até lá agora. Boa sorte.";
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("Level_2");
+    }
+}
