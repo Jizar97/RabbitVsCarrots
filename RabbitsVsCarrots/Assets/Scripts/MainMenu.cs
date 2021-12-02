1acfe4e7e9707ccc328ca0e88397aa6c8d585853
Jizar97 <45200712+Jizar97@users.noreply.github.com>
2021-12-02 00:01:07 -0300

    Introdução removida
    Removi o vídeo de introdução para o projeto ficar jogável, já que o github está bloqueando a criação da build.

--- RabbitsVsCarrots/Assets/Scripts/MainMenu.cs
+++ RabbitsVsCarrots/Assets/Scripts/MainMenu.cs
@@ -5,24 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public Desativador videoPlayer;
-    public Desativador rawImage;
-
     // Start is called before the first frame update
     public void Begin(){
-        GetComponent<AudioSource>().Stop();
-        StartCoroutine(Esperar());
-        
-    }
-
-    public void Exit(){
-        Application.Quit();
-    }
-
-    IEnumerator Esperar(){
-        rawImage.Reativar();
-        videoPlayer.Reativar();
-        yield return new WaitForSeconds(88);
         SceneManager.LoadScene("Level_1");
     }
 }
